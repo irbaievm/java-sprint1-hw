6f218ce8eec6c677cb42991675eea7cbd9db866f
irbaievm <irbaievm@gmail.com>
2021-12-09 01:11:07 +0300

    добавлен импорт файла

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
 
+
+
     public static void main(String[] args) {
-        // Поехали!
+        FileLoader file1 = new FileLoader();
+        String fileData =  file1.readFileContentsOrNull("/Users/irbaiev/dev/java-sprint1-hw/resources/m.202101.csv");
+        System.out.println(fileData);
+
+        }
     }
-}
+
+
+
+
 
